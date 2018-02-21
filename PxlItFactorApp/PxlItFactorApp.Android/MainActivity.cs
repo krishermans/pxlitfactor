a0dfa2536b2d7cd29476bcbcf6d918a0ab33c31b
Kris Hermans <Kris.Hermans@live.be>
2018-02-21 21:37:40 +0100

    experiments with ble

---
+++ PxlItFactorApp/PxlItFactorApp.Android/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android;
+
+namespace PxlItFactorApp.Droid
+{
+    [Activity(Label = "PxlItFactorApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+            this.RequestPermissions(new[]
+            {
+                Manifest.Permission.AccessCoarseLocation,
+                Manifest.Permission.BluetoothPrivileged
+            }, 0);
+        }
+    }
+}
+
---
+++ PxlItFactorApp/PxlItFactorApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Plugin.BluetoothLE;
+using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace PxlItFactorApp
+{
+    public partial class MainPage : ContentPage
+	{
+        private IScanResult scanResult;
+
+        public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void ScanButton_Clicked(object sender, EventArgs args)
+        {
+            CrossBleAdapter.Current.Scan().Subscribe(sr => 
+            {
+                Debug.WriteLine($"found: {sr.Device.Name}");
+                scanResult = sr;
+            });
+        }
+	}
+}
